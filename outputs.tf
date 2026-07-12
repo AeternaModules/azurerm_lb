ba5a5bd3f342f135b18fae9d99944a037e66ac0d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,49 @@
-output "lbs" {
-  description = "All lb resources"
-  value       = azurerm_lb.lbs
-}
 output "lbs_edge_zone" {
-  description = "List of edge_zone values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.edge_zone]
+  description = "Map of edge_zone values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.edge_zone }
 }
 output "lbs_frontend_ip_configuration" {
-  description = "List of frontend_ip_configuration values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.frontend_ip_configuration]
+  description = "Map of frontend_ip_configuration values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.frontend_ip_configuration }
 }
 output "lbs_location" {
-  description = "List of location values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.location]
+  description = "Map of location values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.location }
 }
 output "lbs_name" {
-  description = "List of name values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.name]
+  description = "Map of name values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.name }
 }
 output "lbs_private_ip_address" {
-  description = "List of private_ip_address values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.private_ip_address]
+  description = "Map of private_ip_address values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.private_ip_address }
 }
 output "lbs_private_ip_addresses" {
-  description = "List of private_ip_addresses values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.private_ip_addresses]
+  description = "Map of private_ip_addresses values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.private_ip_addresses }
 }
 output "lbs_public_ip_address_id" {
-  description = "List of public_ip_address_id values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.public_ip_address_id]
+  description = "Map of public_ip_address_id values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.public_ip_address_id }
 }
 output "lbs_resource_group_name" {
-  description = "List of resource_group_name values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.resource_group_name]
+  description = "Map of resource_group_name values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.resource_group_name }
 }
 output "lbs_sku" {
-  description = "List of sku values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.sku]
+  description = "Map of sku values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.sku }
 }
 output "lbs_sku_tier" {
-  description = "List of sku_tier values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.sku_tier]
+  description = "Map of sku_tier values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.sku_tier }
 }
 output "lbs_subnet_id" {
-  description = "List of subnet_id values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.subnet_id]
+  description = "Map of subnet_id values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.subnet_id }
 }
 output "lbs_tags" {
-  description = "List of tags values across all lbs"
-  value       = [for k, v in azurerm_lb.lbs : v.tags]
+  description = "Map of tags values across all lbs, keyed the same as var.lbs"
+  value       = { for k, v in azurerm_lb.lbs : k => v.tags }
 }
 
